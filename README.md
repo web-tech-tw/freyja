56f1e458661c37a4111c9e76ffd8f062a33a172a
不知火 Shiranui <supersonic@livemail.tw>
2024-11-12 04:00:10 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Freyja
+
+The best friend of LINE OpenChat.
+
+LINE 社群加入申請驗證系統
---
+++ app.js
@@ -0,0 +1,103 @@
+"use strict";
+
+require("dotenv").config();
+
+const shortid = require("shortid");
+
+const {
+    CHAT_ROOM_NAME: chatRoomName,
+    CHAT_ROOM_SECRET: chatRoomSecret,
+    CHAT_ROOM_URL: chatRoomUrl,
+    RECAPTCHA_SITE_KEY: reCaptchaSiteKey,
+    RECAPTCHA_SECRET: reCaptchaSecretKey,
+} = process.env;
+
+const express = require("express");
+const basicAuth = require("express-basic-auth");
+const {
+    RecaptchaV2,
+} = require("express-recaptcha");
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+ 
+const adapter = new FileSync("data.json");
+const db = low(adapter);
+
+db.defaults({
+    codes: [],
+}).write();
+  
+const app = express();
+const captcha = new RecaptchaV2(
+    reCaptchaSiteKey, reCaptchaSecretKey,
+);
+
+const adminAuth = basicAuth({
+    challenge: true,
+    users: {
+        "admin": chatRoomSecret,
+    },
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.get("/", (_, res) => {
+    const captchaScript = captcha.render();
+    res.render("index", {
+        chatRoomName,
+        chatRoomUrl,
+        captchaScript,
+    });
+});
+
+app.get("/admin", adminAuth, (_, res) => {
+    res.render("admin", {
+        chatRoomName,
+    });
+});
+
+app.get("/submission", adminAuth, (req, res, next) => {
+    if (!!req.query.code) {
+        next();
+        return;
+    }
+    res.status(400).send("bad request");
+}, (req, res) => {
+    const { code } = req.query;
+    const item = db.get("codes").
+        find((i) => i.code === code).
+        value();
+    if (!item) {
+        res.status(404).send("not found");
+        return;
+    }
+    res.status(200).send(item);
+});
+
+app.post("/submission", captcha.middleware.verify, (req, res, next) => {
+    if (!req.recaptcha.error) {
+        next();
+        return;
+    }
+    res.status(403).send("verification failed");
+}, (_, res) => {
+    const code = shortid.generate();
+    const time = Date.now();
+    db.get("codes").push({
+        time, code,
+    }).write();
+    res.status(201).send({code});
+});
+
+app.listen(3000, () => {
+    console.log(
+        `Freyja is working for "${chatRoomName}" on`,
+        "http://localhost:3000",
+    );
+});
